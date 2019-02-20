6d5894fc505e69bd55259924e6ae2e29c4e1cf51
Bilal Afzaal <bafzaal@uwo.ca>
2019-02-20 18:30:00 -0500

    Enemies Moving Down Screen and Destroyed

--- SE2250_Project/Assets/__Scripts/BoundsCheck.cs
+++ SE2250_Project/Assets/__Scripts/BoundsCheck.cs
@@ -7,10 +7,15 @@ public class BoundsCheck : MonoBehaviour
 
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     void Awake()
     {
@@ -21,20 +26,42 @@ public class BoundsCheck : MonoBehaviour
     void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
 
-        if(pos.x > camWidth - radius)
+        if (pos.x > camWidth - radius)
+        {
             pos.x = camWidth - radius;
+            offRight = true;
+        }
 
         if (pos.x < -camWidth + radius)
+        {
             pos.x = -camWidth + radius;
+            offLeft = true;
+        }
 
         if (pos.y > camHeight - radius)
+        {
             pos.y = camHeight - radius;
+            offUp = true;
+        }
 
         if (pos.y < -camHeight + radius)
+        {
             pos.y = -camHeight + radius;
+            offDown = true;
+        }
+
+        if(keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
 
         transform.position = pos;
+
     }
 
     // Draw the bounds in the Scene pane using OnDrawGizmos()
---
+++ SE2250_Project/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Set In Inspector: Enemy")]
+    public float speed = 10f; // Speed in m/s
+    public float fireRate = 0.3f; // Seconds/shto (Unused)
+    public float health = 10;
+    public int score = 100; // Points earned for destroying this
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    // This is a Property: A method that acts like a field
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if(bndCheck != null && bndCheck.offDown)
+        {
+            // Enemy has gone off the bottom of the screen so destroy it
+            Destroy(gameObject);
+        }
+    }
+}
