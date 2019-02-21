55678f0f5023819557c6351f1cc5beee76730d6a
Bilal Afzaal <bafzaal@uwo.ca>
2019-02-20 23:05:52 -0500

    Distinct Movement For All Enemies

--- SE2250_Project/Assets/__Scripts/Enemy_1.cs
+++ SE2250_Project/Assets/__Scripts/Enemy_1.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Enemy_1 : Enemy
 {
+    bool hitRight = false;
+    
     public override void Move()
     {
         Vector3 tempPos = pos;
         tempPos.y -=  Mathf.Sin(45f) * (speed * Time.deltaTime);
-        tempPos.x += Mathf.Cos(45f) * (speed * Time.deltaTime);
+        if(hitRight == false)
+        {
+            tempPos.x += Mathf.Cos(45f) * (speed * Time.deltaTime);
+        }
+        else
+        {
+            tempPos.x -= Mathf.Cos(45f) * (speed * Time.deltaTime);
+        }
+        if (tempPos.x >= 27f)
+            hitRight = true;
+        if (tempPos.x <= -27)
+            hitRight = false;
+
         pos = tempPos;
     }
 }
--- SE2250_Project/Assets/__Scripts/Enemy_2.cs
+++ SE2250_Project/Assets/__Scripts/Enemy_2.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Enemy_2 : Enemy
 {
+    int numberOfTicks = 0;
+
     public override void Move()
     {
-        base.Move();
+        Vector3 tempPos = pos;
+        tempPos.y -= (speed - 5f) * Time.deltaTime;
+        tempPos.x += Mathf.Sin(tempPos.y);
+
+        pos = tempPos;
     }
 }
