4d7910183649af1433512c5ec2cd061326297eca
jaypols <46539406+jaypols@users.noreply.github.com>
2019-02-21 14:39:54 -0500

    Phase 1: First Commit

---
+++ Space SHUMP Prototype/Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    /// <summary>
+    /// Keeps a GameObject on screen.
+    /// Note that this ONLY works for an orthographic Main Camera at [0,0,0].
+    /// </summary>
+    [Header("Set in Inspector")]
+    public float radius = 1f;
+
+    [Header("Set Dynamically")]
+    public float camWidth;
+    public float camHeight;
+
+    void Awake() { 
+
+        camHeight = Camera.main.orthographicSize; /*accesses the MainCamera tag,
+                                                   makes camHeight the distance from 
+                                                   the origin of the world*/
+      
+        camWidth = camHeight * Camera.main.aspect; /*computes distance from origin
+                                                    to edge of screen */      
+    }
+
+    void LateUpdate() /*(race condition)-called after ever frame after Update() 
+                        to ensure the ship moves to a new position each frame 
+                        before Hero is bounded to screen */       
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+        if (pos.x< -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+
+        transform.position = pos;
+    }
+
+    //Draw the bounds in the Scene pane using OnDrawGizmos()
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+}
---
+++ Space SHUMP Prototype/Assets/__Scripts/Hero.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero          S; //Singleton 
+
+    [Header("Set in Inspector")]
+    //These fields control movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null) {
+            S = this; //set the Singleton
+        }
+        else{
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+   void Update()
+    {
+        //Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        //Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+
+
+    }
+}
