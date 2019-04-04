66c5a786159449eeab3585fc94a9ee0597ec6b8c
Bilal Afzaal <bafzaal@uwo.ca>
2019-04-04 16:29:20 -0400

    Menu Skeleton Added

---
+++ SE2250_Project/Assets/__Scripts/MenuControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+        
+}
